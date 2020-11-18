690523f101c2333f2eb8c20b630cb61c8a348788
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2020-11-17 23:09:12 -0300

    Primeira API Criada!!!!

---
+++ CodersAcademy/CodersAcademy.API/Controllers/AlbumController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodersAcademy.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAlbuns()
+        {
+            return Ok(new
+            {
+                Message = "Primeira API Criada"
+            });
+        }
+
+    }
+}
