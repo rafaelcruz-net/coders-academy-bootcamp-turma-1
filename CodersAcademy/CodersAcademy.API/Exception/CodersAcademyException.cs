627d443386c2a839adff1238e50111ed6e21ac6e
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2020-11-22 21:10:09 -0300

    Resolvendo Code Smells

--- CodersAcademy/CodersAcademy.API/Exception/CodersAcademyException.cs
+++ CodersAcademy/CodersAcademy.API/Exception/CodersAcademyException.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace CodersAcademy.API.Exception
 {
+    [Serializable]
     public class CodersAcademyException : System.Exception
     {
         public CodersAcademyException(string message) : base(message)
+        {
+
+        }
+
+        protected CodersAcademyException(SerializationInfo info, StreamingContext streamingContext ): base(info, streamingContext)
         {
 
         }
